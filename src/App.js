9002a60d84649ec6f07c988829188554a87bd088
Erica <erfrank33@gmail.com>
2023-06-19 12:37:07 -0500

    -added react router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,45 @@ import './App.css'
 import { Home } from './Components/Home.js';
 import { MountainInfo } from './Components/MountainInfo';
 import { NationalParks } from './Components/NationalParks';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+}
+  from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello World
-        
-      </header>
-      <Home />
-      <MountainInfo />
-      <NationalParks />
-    </div>
+    <Router>
+      <div className="App">
+
+        <nav>
+          <ul>
+            <li> <Link to="/">Home </Link></li>
+            <li> <Link to="/">National Parks </Link></li>
+            <li> <Link to="/">Mountain Information </Link></li>
+
+          </ul>
+        </nav>
+
+
+        <header className="App-header">
+
+
+        </header>
+        <Routes>
+          <Route path="/Home">
+            <Home />
+          </Route>
+          <Route path="/MountainInfo">
+            <MountainInfo />
+          </Route>
+          <Route path="National Parks">
+            <NationalParks />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,8 @@
+import './Styles/Home.css'
+import{Link} from 'react-router-dom';
+
+export const Navbar = () => {
+  return (
+    
+  );
+}
